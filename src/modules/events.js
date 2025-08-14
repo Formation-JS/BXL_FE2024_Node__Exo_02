a402bcb439a0ff308ceeabd7f909aa4bf8a69ca3
Gérard Mentor <aurelien.strimelle@bstorm.be>
2025-08-14 12:08:17 +0200

    Exo 02.2 - Refactoring 🧹

--- src/modules/events.js
+++ src/modules/events.js
@@ -4,7 +4,7 @@
  * @returns {Date}
  */
 function getProchaineDate(jour, mois) { 
-    const today = new Date();
+    const today = getToday();
     const nextEvent = new Date(today.getFullYear(), mois - 1, jour);
 
     if(today > nextEvent) {
@@ -14,13 +14,40 @@ function getProchaineDate(jour, mois) {
     return nextEvent
 }
 
+/** Obtenir la date du jours (sans l'heure)
+ * @returns {Date}
+ */
+function getToday() {
+    const today = new Date();
+    today.setHours(0);
+    today.setMinutes(0);
+    today.setSeconds(0);
+    today.setMilliseconds(0);
+    return today;
+}
+
+/** Detecte si l'event est en cours. 
+ * @param {Date} eventDateStart 
+ * @param {Date} eventDateEnd 
+ * @returns {boolean}
+ */
+function detectionEventEnCours(eventDateStart, eventDateEnd) {
+    const today = getToday();
+
+    const isTodayEvent = today.getDate() === eventDateStart.getDate() && today.getMonth() === eventDateStart.getMonth();
+    const inRangeEvent = eventDateEnd !== null 
+                            && eventDateStart.getFullYear() > today.getFullYear()  // Si on a depassé la date de "debut"
+                            && eventDateEnd.getFullYear() === today.getFullYear(); // Est ce qu'an a depassé la date de "fin"
+    
+    return isTodayEvent || inRangeEvent;
+}
 
 /** Permet d'obtenir le nombre de jours avec un event
  * @param {'Noel'|'FeteNat'|'Ete'} event 
  * @returns {number}
  */
 export function getJourAvantEvent(event) {
-    const today = new Date();
+    const today = getToday();
 
     // Date du début et de fin du prochaine event.
     let eventDateStart = null;
@@ -40,16 +67,12 @@ export function getJourAvantEvent(event) {
             throw new Error('Event non supporté !');
     }
 
-
-    const isTodayEvent = today.getDate() === eventDateStart.getDate() && today.getMonth() === eventDateStart.getMonth();
-    const inRangeEvent = eventDateEnd !== null 
-                            && eventDateStart.getFullYear() > today.getFullYear()  // Si on a depassé la date de "debut"
-                            && eventDateEnd.getFullYear() === today.getFullYear(); // Est ce qu'an a depassé la date de "fin"
-    
-    if(isTodayEvent || inRangeEvent) {
+    // Event est en cours ?
+    if(detectionEventEnCours(eventDateStart, eventDateEnd)) {
         return 0;
     }
 
+    // Calcule du nombre de jours 
     const nbMillisecondeDansUnJour = 1000 * 60 * 60 * 24;
     return  Math.ceil((eventDateStart - today) / nbMillisecondeDansUnJour);
 }
